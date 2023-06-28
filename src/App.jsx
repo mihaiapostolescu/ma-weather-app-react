7e48353d493fa7057c07a50f3dd582e3eb5735b1
Mihai Apostolescu <apostolescu.mihaicristian@yahoo.com>
2023-06-28 16:36:31 +0300

    Added 3 columns at 100% for the Main Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import './App.css'
-import { MantineProvider, Text } from '@mantine/core';
+import { MantineProvider, Container, Box, Grid, Navbar } from '@mantine/core';
 
 function App() {
-
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-    <Text>Welcome to Mantine!</Text>
-  </MantineProvider>
-  )
+      <Navbar fixed={false} position={{ top: 0, left: 0 }} mb={10} >
+        <Grid style={{ height: '100%' }}>
+            <Grid.Col span="auto" style={{ color: "blue" }}>span=auto</Grid.Col>
+            <Grid.Col span={3} style={{ textAlign: "center", backgroundColor: "red" }}>span=6</Grid.Col>
+            <Grid.Col span="auto" style={{ textAlign: "right" }}>span=auto</Grid.Col>
+          </Grid>
+      </Navbar>
+
+      <Navbar fixed={false} position={{ top: 0, left: 0 }} >
+        <Grid style={{ height: `100%` }}>
+            <Grid.Col span="auto" style={{ color: "blue"}}>span=auto</Grid.Col>
+            <Grid.Col span={6} style={{ textAlign: "center", backgroundColor: "red", height: "100%"}}>span=6</Grid.Col>
+            <Grid.Col span="auto" style={{ textAlign: "right"}}>span=auto</Grid.Col>
+          </Grid>
+          </Navbar>
+    </MantineProvider>
+  );
 }
 
 export default App
