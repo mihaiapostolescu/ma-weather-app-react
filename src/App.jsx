85b417a2e465a825ac8d31cadf5676eb7f6f4ef5
Mihai Apostolescu <apostolescu.mihaicristian@yahoo.com>
2023-06-29 11:03:27 +0300

    React-accesible-accordion & select-async-paginate

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import './App.css'
-import { MantineProvider, Container, Box, Grid, Navbar } from '@mantine/core';
+import { MantineProvider, Container, Box, Grid, Navbar, Stack, Button, Text, Select } from '@mantine/core';
 
 function App() {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Navbar fixed={false} position={{ top: 0, left: 0 }} mb={10} >
-        <Grid style={{ height: '100%' }}>
-            <Grid.Col span="auto" style={{ color: "blue" }}>span=auto</Grid.Col>
-            <Grid.Col span={3} style={{ textAlign: "center", backgroundColor: "red" }}>span=6</Grid.Col>
+      <Navbar>
+        <Grid style={{ height: '100%', width: '100%' }} p={20}>
+            <Grid.Col span="auto" style={{ color: "blue" }}>  
+            <Text fs="italic">Logo</Text>      
+              <Stack align="left" justify="space-around" h="100%" sx={(theme) => ({ backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] })}>
+                <Text fs="italic"></Text>
+                <Text fs="italic">Current Weather</Text>
+              </Stack>
+            </Grid.Col>
+            <Grid.Col span={5} style={{ textAlign: "center", backgroundColor: "red" }}>
+              <Select
+                placeholder="Search your favorite city here"
+                searchable
+                nothingFound="No options"
+                data={['Timisoara', 'Bucuresti', 'Svelte', 'Vue', 'Iasi']}
+              />
+            </Grid.Col>
             <Grid.Col span="auto" style={{ textAlign: "right" }}>span=auto</Grid.Col>
           </Grid>
       </Navbar>
