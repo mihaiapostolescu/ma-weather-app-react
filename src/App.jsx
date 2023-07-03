eb83e67b43632c56c890dcbe4dde71dccde1bc32
Mihai Apostolescu <apostolescu.mihaicristian@yahoo.com>
2023-07-03 17:15:03 +0300

    Populate DailyForecast & user-select: none for all

--- src/App.jsx
+++ src/App.jsx
@@ -9,99 +9,99 @@ import DailyForecast from './components/current-weather/daily-forecast';
 import { WEATHER_API_KEY, WEATHER_API_URL } from '../geoApi';
 import { useState, useEffect } from 'react';
 
-  function App() {
-    const [currentWeather, setCurrentWeather] = useState(null);
-    const [currentWeatherBackground, setCurrentWeatherBackground] = useState(null);
-    const [hourlyForecast, setHourlyForecast] = useState(null);
-    const [dailyForecast, setDailyForecast] = useState(null);
-  
-    const handleOnSearchChange = (searchData) => {
-      const [lat, lon] = searchData.value.split(" ");
- 
-      const currentWeatherFetch = fetch(`${WEATHER_API_URL}/onecall?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
-      const currentWeatherBackgroundFetch = fetch(`${WEATHER_API_URL}/onecall?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
-      const hourlyForecastFetch = fetch(`${WEATHER_API_URL}/onecall?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
-      const dailyForecastFetch = fetch(`${WEATHER_API_URL}/onecall?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
- 
-      Promise.all([currentWeatherFetch, hourlyForecastFetch, currentWeatherBackgroundFetch, dailyForecastFetch])
+function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [currentWeatherBackground, setCurrentWeatherBackground] = useState(null);
+  const [hourlyForecast, setHourlyForecast] = useState(null);
+  const [dailyForecast, setDailyForecast] = useState(null);
+
+  const handleOnSearchChange = (searchData) => {
+    const [lat, lon] = searchData.value.split(" ");
+
+    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/onecall?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
+    const currentWeatherBackgroundFetch = fetch(`${WEATHER_API_URL}/onecall?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
+    const hourlyForecastFetch = fetch(`${WEATHER_API_URL}/onecall?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
+    const dailyForecastFetch = fetch(`${WEATHER_API_URL}/onecall?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
+
+    Promise.all([currentWeatherFetch, hourlyForecastFetch, currentWeatherBackgroundFetch, dailyForecastFetch])
       .then(async (response) => {
         const weatherResponse = await response[0].json();
         const weatherBackgroundResponse = await response[1].json();
         const hourlyForecastResponse = await response[2].json();
         const dailyForecastResponse = await response[3].json();
- 
-        setCurrentWeather({ city: searchData.label, ...weatherResponse});
-        setCurrentWeatherBackground({ city: searchData.label, ...weatherBackgroundResponse});
-        setHourlyForecast({ city: searchData.label, ...hourlyForecastResponse});
-        setDailyForecast({ city: searchData.label, ...dailyForecastResponse});
+
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setCurrentWeatherBackground({ city: searchData.label, ...weatherBackgroundResponse });
+        setHourlyForecast({ city: searchData.label, ...hourlyForecastResponse });
+        setDailyForecast({ city: searchData.label, ...dailyForecastResponse });
       })
       .catch((err) => console.log(err));
-    }
-  
-    const getCityName = (lat, lon) => {
-      const apiKey = WEATHER_API_KEY;
-      const geoUrl = `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${apiKey}`;
-  
+  }
+
+  const getCityName = (lat, lon) => {
+    const apiKey = WEATHER_API_KEY;
+    const geoUrl = `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${apiKey}`;
+
+    return new Promise((resolve, reject) => {
+      fetch(geoUrl)
+        .then((response) => response.json())
+        .then((geo) => {
+          const cityName = geo[0].name;
+          resolve(cityName);
+        })
+        .catch((error) => {
+          console.error(error);
+          reject(error);
+        });
+    });
+  };
+
+  useEffect(() => {
+    const getCurrentLocation = () => {
       return new Promise((resolve, reject) => {
-        fetch(geoUrl)
-          .then((response) => response.json())
-          .then((geo) => {
-            const cityName = geo[0].name;
-            resolve(cityName);
-          })
-          .catch((error) => {
-            console.error(error);
+        if (!navigator.geolocation) {
+          reject(new Error('Geolocation is not supported by this browser.'));
+          return;
+        }
+
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+            resolve({ latitude, longitude });
+          },
+          (error) => {
             reject(error);
-          });
+          }
+        );
       });
     };
-  
-    useEffect(() => {
-      const getCurrentLocation = () => {
-        return new Promise((resolve, reject) => {
-          if (!navigator.geolocation) {
-            reject(new Error('Geolocation is not supported by this browser.'));
-            return;
-          }
-  
-          navigator.geolocation.getCurrentPosition(
-            (position) => {
-              const latitude = position.coords.latitude;
-              const longitude = position.coords.longitude;
-              resolve({ latitude, longitude });
-            },
-            (error) => {
-              reject(error);
-            }
-          );
-        });
-      };
-  
-      getCurrentLocation()
-        .then((position) => {
-          const { latitude, longitude } = position;
-  
-          getCityName(latitude, longitude)
-            .then((cityName) => {
-              const currentWeatherFetch = fetch(`${WEATHER_API_URL}/onecall?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}&units=metric`);
-              const currentWeatherBackgroundFetch = fetch(`${WEATHER_API_URL}/onecall?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}&units=metric`);
-              const hourlyForecastFetch = fetch(`${WEATHER_API_URL}/onecall?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}&units=metric`);
-              const dailyForecastFetch = fetch(`${WEATHER_API_URL}/onecall?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}&units=metric`);
-  
-              Promise.all([currentWeatherFetch, hourlyForecastFetch, currentWeatherBackgroundFetch, dailyForecastFetch])
-                .then(async (responses) => {
-                  const [weatherResponse, hourlyForecastResponse, weatherBackgroundResponse, dailyForecastResponse] = await Promise.all(responses.map(response => response.json()));
-                  setCurrentWeather({ city: cityName, ...weatherResponse });
-                  setCurrentWeatherBackground({ city: cityName, ...weatherBackgroundResponse });
-                  setHourlyForecast({ city: cityName, ...hourlyForecastResponse });
-                  setDailyForecast({ city: cityName, ...dailyForecastResponse});
-                })
-                .catch((err) => console.log(err));
-            })
-            .catch((error) => console.log(error));
-        })
-        .catch((error) => console.log(error));
-    }, []);
+
+    getCurrentLocation()
+      .then((position) => {
+        const { latitude, longitude } = position;
+
+        getCityName(latitude, longitude)
+          .then((cityName) => {
+            const currentWeatherFetch = fetch(`${WEATHER_API_URL}/onecall?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}&units=metric`);
+            const currentWeatherBackgroundFetch = fetch(`${WEATHER_API_URL}/onecall?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}&units=metric`);
+            const hourlyForecastFetch = fetch(`${WEATHER_API_URL}/onecall?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}&units=metric`);
+            const dailyForecastFetch = fetch(`${WEATHER_API_URL}/onecall?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}&units=metric`);
+
+            Promise.all([currentWeatherFetch, hourlyForecastFetch, currentWeatherBackgroundFetch, dailyForecastFetch])
+              .then(async (responses) => {
+                const [weatherResponse, hourlyForecastResponse, weatherBackgroundResponse, dailyForecastResponse] = await Promise.all(responses.map(response => response.json()));
+                setCurrentWeather({ city: cityName, ...weatherResponse });
+                setCurrentWeatherBackground({ city: cityName, ...weatherBackgroundResponse });
+                setHourlyForecast({ city: cityName, ...hourlyForecastResponse });
+                setDailyForecast({ city: cityName, ...dailyForecastResponse });
+              })
+              .catch((err) => console.log(err));
+          })
+          .catch((error) => console.log(error));
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
 
   console.log(currentWeather);
@@ -109,44 +109,44 @@ import { useState, useEffect } from 'react';
   return (
     <>
 
-    {currentWeatherBackground && <CurrentWeatherBackground data={currentWeatherBackground}/>}
+      {currentWeatherBackground && <CurrentWeatherBackground data={currentWeatherBackground} />}
 
-    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <MantineProvider withGlobalStyles withNormalizeCSS>
 
-     <Header height={{ base: 70, md: 80 }} style={{ width: '100%', zIndex: 4, backgroundColor: "transparent" }} fixed={true} >
-        <div className='opaque'>
-          <Grid style={{ height: '100%', width: '100%', zIndex: 3, minWidth: 350 }} pt={20} pl={20} pb={10} pr={20}>
-              <Grid.Col span="auto" style={{ color: "white" }} pb={20}>  
-              <Text fw={700} pt={10} style={{ textAlign: "left" }}>maweather</Text>      
+        <Header mr={0} height={{ base: 70, md: 80 }} style={{ width: '100%', zIndex: 4, backgroundColor: "transparent" }} fixed={true} >
+          <div className='opaque'>
+            <Grid style={{ height: '100%', width: '100%', zIndex: 3, minWidth: 350 }} pt={20} pl={20} pb={10} pr={20}>
+              <Grid.Col span="auto" style={{ color: "white" }} pb={20}>
+                <Text fw={700} pt={10} style={{ textAlign: "left" }}>maweather</Text>
               </Grid.Col>
-              <Grid.Col span="content" style={{ textAlign: "center"}} md={6}>
+              <Grid.Col span="content" style={{ textAlign: "center" }} md={6}>
                 <div className="container">
-                <Search onSearchChange={handleOnSearchChange}/>
+                  <Search onSearchChange={handleOnSearchChange} />
                 </div>
               </Grid.Col>
               <Grid.Col span="auto" style={{ textAlign: "right" }}></Grid.Col>
             </Grid>
-        </div>
-      </Header>
-   
-      <Navbar>
-        <Grid style={{ height: '100%', width: '100%', zIndex: 3 }} p={20}>
-            <Grid.Col span="auto" style={{ color: "white" }} pb={20}>  
-            <Text fs="bold" pt={10}></Text>      
+          </div>
+        </Header>
+
+        <Navbar mr={0}>
+          <Grid style={{ height: '100%', width: '100%', zIndex: 3 }} p={20}>
+            <Grid.Col span="auto" style={{ color: "white" }} pb={20}>
+              <Text fs="bold" pt={10}></Text>
               <Stack align="left" justify="space-around" h="130%">
-              {/* <Stack align="left" justify="space-around" h="100%" sx={(theme) => ({ backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[8] })}> */}
+                {/* <Stack align="left" justify="space-around" h="100%" sx={(theme) => ({ backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[8] })}> */}
                 <Text fs="italic"></Text>
                 {/* <Search onSearchChange={handleOnSearchChange} /> */}
                 {currentWeather && <CurrentWeather data={currentWeather} />}
               </Stack>
             </Grid.Col>
-            <Grid.Col span="content" style={{ textAlign: "center"}} md={6}>
+            <Grid.Col span="content" style={{ textAlign: "center" }} md={6}>
             </Grid.Col>
             <Grid.Col span="auto" style={{ textAlign: "right" }}></Grid.Col>
           </Grid>
-      </Navbar>
+        </Navbar>
 
-      {/* <Footer fixed={false} style={{ width: '100%', zIndex: 4, backgroundColor: "white" }}  >
+        {/* <Footer fixed={false} style={{ width: '100%', zIndex: 4, backgroundColor: "white" }}  >
         <div>
           <Grid style={{ height: '100%', width: '100%' }} p={20}>
             <Grid.Col span="auto" style={{ color: "blue"}}>span=auto</Grid.Col>
@@ -156,26 +156,24 @@ import { useState, useEffect } from 'react';
         </div>
       </Footer> */}
 
-      <Footer mt={100} style={{ width: '98%', zIndex: 5, color: "white" }} >
-        <Flex
-          mih={50}
-          bg="rgba(0, 0, 0, .3)"
-          gap="md"
-          justify="flex-start"
-          align="flex-start"
-          direction="column"
-          wrap="wrap"
-          width="100"
-          zIndex="6"
-          fw="700"
-         >
-          {hourlyForecast && <HourlyForecast data={hourlyForecast}/>}
-          
-          {dailyForecast && <DailyForecast data={dailyForecast}/>}
-          
-        </Flex>
-      </Footer>
-    </MantineProvider>
+        <Footer mt={100} pr={30} style={{ width: '100%', zIndex: 5, color: "white" } } >
+          <Flex
+            mih={50}
+            bg="rgba(0, 0, 0, .3)"
+            gap="md"
+            justify="flex-start"
+            align="flex-start"
+            direction="column"
+            wrap="wrap"
+            width="100"
+          >
+            {hourlyForecast && <HourlyForecast data={hourlyForecast} />}
+
+            {dailyForecast && <DailyForecast data={dailyForecast} />}
+
+          </Flex>
+        </Footer>
+      </MantineProvider>
     </>
   );
 }
--- src/components/current-weather/daily-forecast.jsx
+++ src/components/current-weather/daily-forecast.jsx
@@ -1,197 +1,100 @@
 import "./daily-hourly-forecast.css"
 
-const DailyForecast = ({data}) => {
-    for (let i = 0; i <= 4; i++) {
-      const dailyTimeStamp = data.daily[i].dt
-      const dailyDate = new Date(dailyTimeStamp * 1000)
-      const options = {
-        weekday: "long",
-        // year: "numeric",
-        month: "long",
-        day: "numeric",
+const DailyForecast = ({ data }) => {
+  const dailyForecastItems = [];
+
+  for (let i = 0; i <= 5; i++) {
+    const dailyTimeStamp = data.daily[i].dt
+    const dailyDate = new Date(dailyTimeStamp * 1000)
+    const options = {
+      weekday: "long",
+      // year: "numeric",
+      // month: "long",
+      // day: "numeric",
+    }
+    const dailyTimeString = dailyDate.toLocaleString("en-US", options)
+    let dataDailyMain = data.daily[i].weather[0].main
+    let dataDailyDescription = data.daily[i].weather[0].description
+    let pictureSource
+    let AtmosphereWeather = [
+      "Mist",
+      "Smoke",
+      "Haze",
+      "Dust",
+      "Fog",
+      "Sand",
+      "Dust",
+      "Ash",
+      "Squall",
+    ]
+
+    if (dataDailyMain === "Clear") {
+      pictureSource = "https://res.cloudinary.com/dewznnjqr/image/upload/v1681134909/maweatherapp/icon-clear-sky-day_yrsvbs.png"
+    } else if (dataDailyMain === "Clouds") {
+      if (dataDailyDescription === "scattered clouds") {
+        pictureSource = "https://res.cloudinary.com/dewznnjqr/image/upload/v1681134910/maweatherapp/icon-scattered-clouds-day_psh3zf.png"
+      } else if (dataDailyDescription === "few clouds") {
+        pictureSource = "https://res.cloudinary.com/dewznnjqr/image/upload/v1681134909/maweatherapp/icon-few-clouds-day_gw7apg.png"
+      } else if (dataDailyDescription === "broken clouds") {
+        pictureSource = "https://res.cloudinary.com/dewznnjqr/image/upload/v1681207506/maweatherapp/icon-broken-clouds-day_x3xuy8.png"
+      } else if (dataDailyDescription === "overcast clouds") {
+        pictureSource = "https://res.cloudinary.com/dewznnjqr/image/upload/v1681207506/maweatherapp/icon-broken-clouds-day_x3xuy8.png"
       }
-      const dailyTimeString = dailyDate.toLocaleString("en-US", options)
-      let dataDailyMain = data.daily[i].weather[0].main
-      let dataDailyDescription = data.daily[i].weather[0].description
-      let pictureSource
-      let AtmosphereWeather = [
-        "Mist",
-        "Smoke",
-        "Haze",
-        "Dust",
-        "Fog",
-        "Sand",
-        "Dust",
-        "Ash",
-        "Squall",
-      ]
-  
-      if (dataDailyMain === "Clear") {
-        let pictureSource = "https://res.cloudinary.com/dewznnjqr/image/upload/v1681134909/maweatherapp/icon-clear-sky-day_yrsvbs.png"
-      } else if (dataDailyMain === "Clouds") {
-        if (dataDailyDescription === "scattered clouds") {
-          let pictureSource = "https://res.cloudinary.com/dewznnjqr/image/upload/v1681134910/maweatherapp/icon-scattered-clouds-day_psh3zf.png"
-
-        } else if (dataDailyDescription === "few clouds") {
-          let pictureSource = "https://res.cloudinary.com/dewznnjqr/image/upload/v1681134909/maweatherapp/icon-few-clouds-day_gw7apg.png"
-
-        } else if (dataDailyDescription === "broken clouds") {
-          let pictureSource = "https://res.cloudinary.com/dewznnjqr/image/upload/v1681207506/maweatherapp/icon-broken-clouds-day_x3xuy8.png"
-
-        } else if (dataDailyDescription === "overcast clouds"){
-          let pictureSource = "https://res.cloudinary.com/dewznnjqr/image/upload/v1681207506/maweatherapp/icon-broken-clouds-day_x3xuy8.png"
-
-        }
-      } else if (AtmosphereWeather.includes(dataDailyMain)) {
-        let pictureSource = "https://res.cloudinary.com/dewznnjqr/image/upload/v1681134910/maweatherapp/icon-mist-day_z33ury.png"
-      } else if (dataDailyMain === "Tornado") {
-        let pictureSource = "https://res.cloudinary.com/dewznnjqr/image/upload/v1681156605/maweatherapp/icon-tornado-day_ackxb9.png"
-      } else if (dataDailyMain === "Snow") {
-        let pictureSource = "https://res.cloudinary.com/dewznnjqr/image/upload/v1681134911/maweatherapp/icon-snow-day_ol7kgq.png"
-      } else if (dataDailyMain === "Rain") {
-        let pictureSource = "https://res.cloudinary.com/dewznnjqr/image/upload/v1681134911/maweatherapp/icon-shower-rain-day_zij6wr.png"
-      } else if (dataDailyMain === "Drizzle") {
-        let pictureSource = "https://res.cloudinary.com/dewznnjqr/image/upload/v1681134909/maweatherapp/icon-drizzle-day_vmfudt.png"
-      } else if (dataDailyMain === "Thunderstorm") {
-        let pictureSource = "https://res.cloudinary.com/dewznnjqr/image/upload/v1681134911/maweatherapp/icon-thunderstorm-day_svukzl.png"
-      }
+    } else if (AtmosphereWeather.includes(dataDailyMain)) {
+      pictureSource = "https://res.cloudinary.com/dewznnjqr/image/upload/v1681134910/maweatherapp/icon-mist-day_z33ury.png"
+    } else if (dataDailyMain === "Tornado") {
+      pictureSource = "https://res.cloudinary.com/dewznnjqr/image/upload/v1681156605/maweatherapp/icon-tornado-day_ackxb9.png"
+    } else if (dataDailyMain === "Snow") {
+      pictureSource = "https://res.cloudinary.com/dewznnjqr/image/upload/v1681134911/maweatherapp/icon-snow-day_ol7kgq.png"
+    } else if (dataDailyMain === "Rain") {
+      pictureSource = "https://res.cloudinary.com/dewznnjqr/image/upload/v1681134911/maweatherapp/icon-shower-rain-day_zij6wr.png"
+    } else if (dataDailyMain === "Drizzle") {
+      pictureSource = "https://res.cloudinary.com/dewznnjqr/image/upload/v1681134909/maweatherapp/icon-drizzle-day_vmfudt.png"
+    } else if (dataDailyMain === "Thunderstorm") {
+      pictureSource = "https://res.cloudinary.com/dewznnjqr/image/upload/v1681134911/maweatherapp/icon-thunderstorm-day_svukzl.png"
     }
 
-    return (
-        <div className="daily-forecast flexc">
-        <p className="daily-forecast-title">Daily Forecast</p>
-        <div className="fived-forecast flexr">
-          <div className="today flexc">
-            <p className="forecast-day">Today</p>
-            <div className="temp-day-night flexr centered">
-              <p className="forecast-temperature-day" title="During day">temp</p>
-              <p className="backslash">/</p>
-              <p className="forecast-temperature-night" title="During night">
-                temp
-              </p>
-            </div>
-            <img
-              src="https://res.cloudinary.com/dewznnjqr/image/upload/v1681134909/maweatherapp/icon-clear-sky-day_yrsvbs.png"
-              className="forecast-weather-day"
-              alt="Icon for weather forecast"
-              title="weather forecast"
-            />
-            <div className="humidity flexr centered">
-              <p className="forecast-humidity-day">humidity</p>
-              <img
-                src="https://res.cloudinary.com/dewznnjqr/image/upload/v1681134910/maweatherapp/icon-humidity_unabmr.png"
-                className="icon-humidity"
-                alt="Icon of humidity"
-                title="humidity"
-              />
-            </div>
+
+    dailyForecastItems.push(
+      <div className="forecast-item" key={i}>
+        <div className="today flexc">
+          <p className="forecast-day">{dailyTimeString}</p>
+          <div className="temp-day-night flexr centered">
+            <p className="forecast-temperature-day" title="During day">{Math.round(data.daily[i].temp.day)}</p>
+            <p className="backslash">/</p>
+            <p className="forecast-temperature-night" title="During night">{Math.round(data.daily[i].temp.night)}</p>
           </div>
-          <div className="plus1d flexc">
-            <p className="forecast-day">plus1d</p>
-            <div className="temp-day-night flexr centered">
-              <p className="forecast-temperature-day" title="During day">temp</p>
-              <p className="backslash">/</p>
-              <p className="forecast-temperature-night" title="During night">
-                temp
-              </p>
-            </div>
+          <img
+            src={pictureSource}
+            className="forecast-weather-day"
+            alt="Icon for weather forecast"
+            title={dataDailyDescription}
+          />
+          <div className="humidity flexr centered">
+            <p className="forecast-humidity-day">{data.daily[i].humidity}</p>
             <img
-              src="https://res.cloudinary.com/dewznnjqr/image/upload/v1681134909/maweatherapp/icon-clear-sky-day_yrsvbs.png"
-              className="forecast-weather-day"
-              alt="Icon for weather forecast"
-              title="weather forecast"
+              src="https://res.cloudinary.com/dewznnjqr/image/upload/v1681134910/maweatherapp/icon-humidity_unabmr.png"
+              className="icon-humidity"
+              alt="Icon of humidity"
+              title="humidity"
             />
-            <div className="humidity flexr centered">
-              <p className="forecast-humidity-day">humidity</p>
-              <img
-                src="https://res.cloudinary.com/dewznnjqr/image/upload/v1681134910/maweatherapp/icon-humidity_unabmr.png"
-                className="icon-humidity"
-                alt="Icon of humidity"
-                title="humidity"
-              />
-            </div>
-          </div>
-          <div className="plus2d flexc">
-            <p className="forecast-day">plus2d</p>
-            <div className="temp-day-night flexr centered">
-              <p className="forecast-temperature-day" title="During day">temp</p>
-              <p className="backslash">/</p>
-              <p className="forecast-temperature-night" title="During night">
-                temp
-              </p>
-            </div>
-            <img
-              src="https://res.cloudinary.com/dewznnjqr/image/upload/v1681134909/maweatherapp/icon-clear-sky-day_yrsvbs.png"
-              className="forecast-weather-day"
-              alt="Icon for weather forecast"
-              title="weather forecast"
-            />
-            <div className="humidity flexr centered">
-              <p className="forecast-humidity-day">humidity</p>
-              <img
-                src="https://res.cloudinary.com/dewznnjqr/image/upload/v1681134910/maweatherapp/icon-humidity_unabmr.png"
-                className="icon-humidity"
-                alt="Icon of humidity"
-                title="humidity"
-              />
-            </div>
-          </div>
-          <div className="plus3d flexc">
-            <p className="forecast-day">plus3d</p>
-            <div className="temp-day-night flexr centered">
-              <p className="forecast-temperature-day" title="During day">temp</p>
-              <p className="backslash">/</p>
-              <p className="forecast-temperature-night" title="During night">
-                temp
-              </p>
-            </div>
-            <img
-              src="https://res.cloudinary.com/dewznnjqr/image/upload/v1681134909/maweatherapp/icon-clear-sky-day_yrsvbs.png"
-              className="forecast-weather-day"
-              alt="Icon for weather forecast"
-              title="weather forecast"
-            />
-            <div className="humidity flexr centered">
-              <p className="forecast-humidity-day">humidity</p>
-              <img
-                src="https://res.cloudinary.com/dewznnjqr/image/upload/v1681134910/maweatherapp/icon-humidity_unabmr.png"
-                className="icon-humidity"
-                alt="Icon of humidity"
-                title="humidity"
-              />
-            </div>
-          </div>
-          <div className="plus4d flexc">
-            <p className="forecast-day">plus4d</p>
-            <div className="temp-day-night flexr centered">
-              <p className="forecast-temperature-day" title="During day">temp</p>
-              <p className="backslash">/</p>
-              <p className="forecast-temperature-night" title="During night">
-                temp
-              </p>
-            </div>
-            <img
-              src="https://res.cloudinary.com/dewznnjqr/image/upload/v1681134909/maweatherapp/icon-clear-sky-day_yrsvbs.png"
-              className="forecast-weather-day"
-              alt="Icon for weather forecast"
-              title="weather forecast"
-            />
-            <div className="humidity flexr centered">
-              <p className="forecast-humidity-day">humidity</p>
-              <img
-                src="https://res.cloudinary.com/dewznnjqr/image/upload/v1681134910/maweatherapp/icon-humidity_unabmr.png"
-                className="icon-humidity"
-                alt="Icon of humidity"
-                title="humidity"
-              />
-            </div>
           </div>
         </div>
-        <button>
-          <p className="next-seven-days">Next 7 Days</p>
-        </button>
       </div>
-    )
+    );
   }
 
-  export default DailyForecast;
+  return (
+    <div className="daily-forecast flexc">
+      <p className="daily-forecast-title">Daily Forecast</p>
+      <div className="fived-forecast flexr">
+        {dailyForecastItems}
+      </div>
+      <button>
+        <p className="next-seven-days">Next 7 Days</p>
+      </button>
+    </div>
+  )
+}
+
+export default DailyForecast;
